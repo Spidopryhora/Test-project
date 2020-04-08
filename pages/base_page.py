28d18960d3cd0fc301f8b80aff12a07c5b3c6bd0
spidopryhora <bizserp@gmail.com>
2020-04-08 20:27:42 +0300

    Added LoginPage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,12 @@
 from selenium.webdriver import Remote
 from selenium.common.exceptions import NoSuchElementException
 
+
 class BasePage:
-    def __init__(self, browser: Remote, url, timeout = 10):
+    def __init__(self, browser: Remote, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
-
 
     def open(self):
         self.browser.get(self.url)
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,13 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.ID, "login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.ID, 'login_link'), 'Login link is not present'
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Login link is not present'
