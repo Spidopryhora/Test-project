dfbe79de3f435fdde259504fa73eed8b6d4cbf6e
spidopryhora <bizserp@gmail.com>
2020-04-10 12:42:11 +0300

    Added testing for parametrized promo

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,12 @@
 from pages.product_page import ProductPage
 import time
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/uk/catalogue/coders-at-work_207/?promo=newYear2019'
+@pytest.mark.parametrize('promo_offer', ['0', '1', '2', '3', '4', '5', '6', pytest.param('7', marks=pytest.mark.xfail),
+                                         '8', '9'])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f'http://selenium1py.pythonanywhere.com/uk/catalogue/coders-at-work_207/?promo=offer{promo_offer}'
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_cart()
